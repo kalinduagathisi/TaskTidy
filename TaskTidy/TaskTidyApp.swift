e6bd8599f187590d3e6e6216faf59475b1107f2d
kalinduagathisi <kalinduagathisi@gmail.com>
2024-11-18 08:31:17 +0530

    seperated files to mvvm architechture

--- TaskTidy/TaskTidyApp.swift
+++ TaskTidy/TaskTidyApp.swift
@@ -5,10 +5,15 @@
 //  Created by Kalindu Agathisi on 2024-11-17.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct TaskTidyApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
